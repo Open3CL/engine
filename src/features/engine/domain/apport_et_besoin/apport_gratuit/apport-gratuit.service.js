98d51d6af4bf17e8b55ba980cb0cc3776b2b861d
Jérôme GAVIGNET <jerome.gavignet@redfroggy.fr>
2025-03-04 16:02:18 +0100

    refactor: besoin fr and apport gratuit service refactoring

---
+++ src/features/engine/domain/apport_et_besoin/apport_gratuit/apport-gratuit.service.js
@@ -0,0 +1,134 @@
+import { inject } from 'dioma';
+import { SurfaceSudEquivalenteService } from '../surface-sud-equivalente.service.js';
+import { mois_liste } from '../../../../../utils.js';
+import { FrTvStore } from '../../../../dpe/infrastructure/froid/frTv.store.js';
+
+/**
+ * Calcul des apports gratuits
+ * Chapitre 6 Détermination des apports gratuits
+ *
+ * Methode_de_calcul_3CL_DPE_2021 - Page 42
+ * Octobre 2021
+ * @see consolide_anne…arrete_du_31_03_2021_relatif_aux_methodes_et_procedures_applicables.pdf
+ */
+export class ApportGratuitService {
+  /**
+   * @type {FrTvStore}
+   */
+  #frTvStore;
+
+  /**
+   * @type {SurfaceSudEquivalenteService}
+   */
+  #surfaceSudEquivalenteService;
+
+  /**
+   * @param frTvStore {FrTvStore}
+   * @param surfaceSudEquivalenteService {SurfaceSudEquivalenteService}
+   */
+  constructor(
+    frTvStore = inject(FrTvStore),
+    surfaceSudEquivalenteService = inject(SurfaceSudEquivalenteService)
+  ) {
+    this.#frTvStore = frTvStore;
+    this.#surfaceSudEquivalenteService = surfaceSudEquivalenteService;
+  }
+
+  /**
+   * Apports solaires gratuits du logement
+   *
+   * @param ctx {Contexte}
+   * @param logement {Logement}
+   * @returns {number}
+   */
+  apportSolaire(ctx, logement) {
+    const clim = logement.climatisation_collection?.climatisation || [];
+
+    return mois_liste.reduce(
+      (acc, mois) => {
+        acc.apport_solaire_ch += this.apportSolaireMois(
+          ctx,
+          logement.enveloppe,
+          mois,
+          this.#frTvStore.getData(
+            'e',
+            ctx.altitude.value,
+            ctx.zoneClimatique.value,
+            mois,
+            ctx.inertie.ilpa
+          )
+        );
+        if (clim.length > 0) {
+          acc.apport_solaire_fr += this.apportSolaireMois(
+            ctx,
+            logement.enveloppe,
+            mois,
+            this.#frTvStore.getData('e_fr_28', ctx.altitude.value, ctx.zoneClimatique.value, mois)
+          );
+        }
+        return acc;
+      },
+      { apport_solaire_ch: 0, apport_solaire_fr: 0 }
+    );
+  }
+
+  /**
+   * Apports internes gratuits du logement
+   *
+   * @param ctx {Contexte}
+   * @param logement {Logement}
+   * @returns {number}
+   */
+  apportInterne(ctx, logement) {
+    const clim = logement.climatisation_collection?.climatisation || [];
+
+    return mois_liste.reduce(
+      (acc, mois) => {
+        acc.apport_interne_ch += this.apportInterneMois(
+          ctx,
+          this.#frTvStore.getData(
+            'nref19',
+            ctx.altitude.value,
+            ctx.zoneClimatique.value,
+            mois,
+            ctx.inertie.ilpa
+          )
+        );
+        if (clim.length > 0) {
+          acc.apport_interne_fr += this.apportInterneMois(
+            ctx,
+            this.#frTvStore.getData('nref28', ctx.altitude.value, ctx.zoneClimatique.value, mois)
+          );
+        }
+        return acc;
+      },
+      { apport_interne_ch: 0, apport_interne_fr: 0 }
+    );
+  }
+
+  /**
+   * Apports solaires pour un mois donné
+   *
+   * @param ctx {Contexte}
+   * @param enveloppe {Enveloppe}
+   * @param mois {string}
+   * @param e {number} ensoleillement reçu, pour le mois, par une paroi verticale orientée au sud en absence d'ombrage (kWh/m²)
+   * @returns {number}
+   */
+  apportSolaireMois(ctx, enveloppe, mois, e) {
+    return 1000 * this.#surfaceSudEquivalenteService.ssdMois(ctx, enveloppe, mois) * e;
+  }
+
+  /**
+   * Apports internes dans le logement pour un mois donné
+   * Les apports internes de chaleur dus aux équipements prennent en compte l’ensemble des équipements
+   * « mobiliers » (cuisson, audiovisuel, informatique, lavage, froid, appareils ménagers)
+   *
+   * @param ctx {Contexte}
+   * @param nref {number} nombre d’heures de chauffage pour le mois
+   * @returns {number}
+   */
+  apportInterneMois(ctx, nref) {
+    return ((3.18 + 0.34) * ctx.surfaceHabitable + 90 * (132 / 168) * ctx.nadeq) * nref;
+  }
+}
---
+++ src/features/engine/domain/apport_et_besoin/apport_gratuit/apport-gratuit.service.spec.js
@@ -0,0 +1,167 @@
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { SurfaceSudEquivalenteService } from './../surface-sud-equivalente.service.js';
+import { ApportGratuitService } from './apport-gratuit.service.js';
+import { FrTvStore } from '../../../../dpe/infrastructure/froid/frTv.store.js';
+import { mois_liste } from '../../../../../utils.js';
+
+/** @type {FrTvStore} **/
+let frTvStore;
+
+/** @type {SurfaceSudEquivalenteService} **/
+let surfaceSudEquivalenteService;
+
+/** @type {ApportGratuitService} **/
+let service;
+
+describe('Calcul des apports gratuits au logement', () => {
+  beforeEach(() => {
+    frTvStore = new FrTvStore();
+    surfaceSudEquivalenteService = new SurfaceSudEquivalenteService();
+    service = new ApportGratuitService(frTvStore, surfaceSudEquivalenteService);
+  });
+
+  test.each([
+    {
+      label: 'avec climatisation',
+      withClimatisation: true
+    },
+    {
+      label: 'sans climatisation',
+      withClimatisation: false
+    }
+  ])('Determination des apports gratuits pour un logement $label', ({ withClimatisation }) => {
+    vi.spyOn(surfaceSudEquivalenteService, 'ssdMois').mockReturnValue(18.5);
+    vi.spyOn(frTvStore, 'getData').mockReturnValue(10);
+
+    /** @type {Contexte} */
+    const ctx = {
+      zoneClimatique: { value: 'h1a' },
+      altitude: { value: '400-800m' },
+      inertie: { ilpa: 1 }
+    };
+
+    /** @type { Logement } **/
+    const logement = { enveloppe: { porte_collection: {} } };
+
+    if (withClimatisation) {
+      logement.climatisation_collection = { climatisation: [{}] };
+    }
+
+    expect(service.apportSolaire(ctx, logement)).toStrictEqual({
+      apport_solaire_ch: 2220000,
+      apport_solaire_fr: withClimatisation ? 2220000 : 0
+    });
+    expect(surfaceSudEquivalenteService.ssdMois).toHaveBeenCalledWith(
+      ctx,
+      logement.enveloppe,
+      'Janvier'
+    );
+
+    for (const mois of mois_liste) {
+      expect(frTvStore.getData).toHaveBeenCalledWith(
+        'e',
+        ctx.altitude.value,
+        ctx.zoneClimatique.value,
+        mois,
+        ctx.inertie.ilpa
+      );
+
+      if (withClimatisation) {
+        expect(frTvStore.getData).toHaveBeenCalledWith(
+          'e_fr_28',
+          ctx.altitude.value,
+          ctx.zoneClimatique.value,
+          mois
+        );
+      } else {
+        expect(frTvStore.getData).not.toHaveBeenCalledWith(
+          'e_fr_28',
+          ctx.altitude.value,
+          ctx.zoneClimatique.value,
+          mois
+        );
+      }
+    }
+  });
+
+  test.each([
+    {
+      label: 'avec climatisation',
+      withClimatisation: true
+    },
+    {
+      label: 'sans climatisation',
+      withClimatisation: false
+    }
+  ])('Determination des apports internes pour un logement $label', ({ withClimatisation }) => {
+    vi.spyOn(frTvStore, 'getData').mockReturnValue(10);
+
+    /** @type {Contexte} */
+    const ctx = {
+      zoneClimatique: { value: 'h1a' },
+      altitude: { value: '400-800m' },
+      inertie: { ilpa: 1 },
+      surfaceHabitable: 12,
+      nadeq: 1.25
+    };
+
+    /** @type { Logement } **/
+    const logement = { enveloppe: { porte_collection: {} } };
+
+    if (withClimatisation) {
+      logement.climatisation_collection = { climatisation: [{}] };
+    }
+
+    expect(service.apportInterne(ctx, logement)).toStrictEqual({
+      apport_interne_ch: 15675.94285714286,
+      apport_interne_fr: withClimatisation ? 15675.94285714286 : 0
+    });
+
+    for (const mois of mois_liste) {
+      expect(frTvStore.getData).toHaveBeenCalledWith(
+        'nref19',
+        ctx.altitude.value,
+        ctx.zoneClimatique.value,
+        mois,
+        ctx.inertie.ilpa
+      );
+
+      if (withClimatisation) {
+        expect(frTvStore.getData).toHaveBeenCalledWith(
+          'nref28',
+          ctx.altitude.value,
+          ctx.zoneClimatique.value,
+          mois
+        );
+      } else {
+        expect(frTvStore.getData).not.toHaveBeenCalledWith(
+          'nref28',
+          ctx.altitude.value,
+          ctx.zoneClimatique.value,
+          mois
+        );
+      }
+    }
+  });
+
+  test('Determination des apports solaires pour un mois donné', () => {
+    vi.spyOn(surfaceSudEquivalenteService, 'ssdMois').mockReturnValue(18.5);
+
+    /** @type {Contexte} */
+    const ctx = { zoneClimatique: { id: 1 } };
+
+    /** @type { Enveloppe } **/
+    const enveloppe = {
+      porte_collection: {}
+    };
+    expect(service.apportSolaireMois(ctx, enveloppe, 'Janvier', 18)).toBe(333000);
+    expect(surfaceSudEquivalenteService.ssdMois).toHaveBeenCalledWith(ctx, enveloppe, 'Janvier');
+  });
+
+  test('Determination des apports internes dans le logement pour un mois donné', () => {
+    /** @type {Contexte} */
+    const ctx = { surfaceHabitable: 88, nadeq: 2 };
+
+    expect(service.apportInterneMois(ctx, 18)).toBeCloseTo(8121.39, 2);
+  });
+});
