9e7fccbd5eab78c5d5e21c38a76f9df95a8436e0
Michael Desigaud <michael.desigaud@redfroggy.fr>
2025-06-10 12:01:43 +0200

    feat: export classe dpe ges function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { calcul_3cl } from './src/index.js';
-export { calcul_3cl };
+import { calcul_3cl, get_classe_ges_dpe } from './src/index.js';
+export { calcul_3cl, get_classe_ges_dpe };
 import { Umur, Uph, Upb, Uporte, Ubv, Upt, calc_deperdition } from './src/3_deperdition.js';
 export { Umur, Uph, Upb, Uporte, Ubv, Upt, calc_deperdition };
