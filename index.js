3a277c37317c44933b18c41f739817ac00cc4518
Kaiohz <yohan.goncalves@cosigma.io>
2025-07-07 19:24:58 +0200

    fix: import fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { calcul_3cl, get_classe_ges_dpe } from './src/index.js';
 export { calcul_3cl, get_classe_ges_dpe };
-import { Umur, Uph, Upb, Uporte, Ubv, Upt, calc_deperdition } from './src/3_deperdition.js';
+import { Umur, Uph, Upb, Uporte, Ubv, Upt } from './src/3_deperdition.js';
+import calc_deperdition from './src/3_deperdition.js';
 export { Umur, Uph, Upb, Uporte, Ubv, Upt, calc_deperdition };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { calcul_3cl, get_classe_ges_dpe } from './engine.js';
 export { calcul_3cl, get_classe_ges_dpe };
 import { Umur, Uph, Upb, Uporte, Ubv, Upt } from './3_deperdition.js';
-export { Umur, Uph, Upb, Uporte, Ubv, Upt };
+import calc_deperdition from './3_deperdition.js';
+export { Umur, Uph, Upb, Uporte, Ubv, Upt, calc_deperdition };
