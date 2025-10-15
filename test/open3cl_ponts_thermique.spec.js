55e0d4ad0e2f2f6ea25435bed7931cfaa2007d31
Michael Desigaud <michael.desigaud@redfroggy.fr>
2025-10-15 15:07:04 +0200

    fix: issue-107 - calcul pt si k_saisi=0

---
+++ test/open3cl_ponts_thermique.spec.js
@@ -0,0 +1,23 @@
+import { calcul_3cl } from '../src/engine.js';
+import corpus from './corpus.json';
+import { getAdemeFileJson } from './test-helpers.js';
+import { set_tv_match_optimized_version, unset_tv_match_optimized_version } from '../src/utils.js';
+import { describe, expect, test } from 'vitest';
+
+describe('Ponts thermiques unit tests', () => {
+  test('should calculate deperdition if k_saisi=0', () => {
+    let input = getAdemeFileJson('2569E3101728B');
+    const inputPt =
+      input.logement.enveloppe.pont_thermique_collection.pont_thermique[2].donnee_intermediaire.k;
+
+    /** @type {FullDpe} **/
+    let output = calcul_3cl(structuredClone(input));
+    const outputPt =
+      output.logement.enveloppe.pont_thermique_collection.pont_thermique[2].donnee_intermediaire.k;
+
+    expect(inputPt).toEqual(outputPt);
+    expect(input.logement.sortie.deperdition.deperdition_pont_thermique).toBeCloseTo(
+      output.logement.sortie.deperdition.deperdition_pont_thermique
+    );
+  });
+});
