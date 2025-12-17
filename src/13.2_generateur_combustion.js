45d59a03c400b14e27a1b29f5f19acfd941b64a5
Michael Desigaud <michael.desigaud@redfroggy.fr>
2025-12-17 17:01:51 +0100

    chore: update reports output

--- src/13.2_generateur_combustion.js
+++ src/13.2_generateur_combustion.js
@@ -67,7 +67,17 @@ function evaluateFormula(formulaOrValue, pn, E, F) {
  * @param tbase {number} température de fonctionnement du générateur
  * @param methodeSaisie {number} méthode de saisie des caractéristiques du générateur
  */
-export function tv_generateur_combustion(dpe, di, de, type, GV, tbase, methodeSaisie) {
+export function tv_generateur_combustion(
+  dpe,
+  di,
+  de,
+  type,
+  GV,
+  tbase,
+  methodeSaisie,
+  th,
+  installCh
+) {
   const typeGenerateurKey = `enum_type_generateur_${type}_id`;
   let enumTypeGenerateurId = de[typeGenerateurKey];
   let row;
