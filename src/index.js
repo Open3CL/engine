54e98dfad2f5973cb5a1a1579f1c65b71464b5c3
Michael Desigaud <michael.desigaud@redfroggy.fr>
2026-09-01 18:11:40 +0200

    fix: invalid method get_conso_coeff_2_3_206 import

--- src/index.js
+++ src/index.js
@@ -6,13 +6,6 @@ import {
   get_conso_coeff_1_7_2027,
   getVersion
 } from './engine.js';
-export {
-  calcul_3cl,
-  calcul_3cl_xml,
-  get_classe_ges_dpe,
-  get_conso_coeff_1_9_2026,
-  get_conso_coeff_1_7_2027,
-  getVersion
-};
+export { calcul_3cl, calcul_3cl_xml, get_classe_ges_dpe, get_conso_coeff_1_7_2027, getVersion };
 import { Umur, Uph, Upb, Uporte, Ubv, Upt } from './3_deperdition.js';
 export { Umur, Uph, Upb, Uporte, Ubv, Upt };
